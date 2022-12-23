d44404ceb5b4080a9e7a0c6758a8c1bd21ad2493
Adriegrip <80099106+Adriegrip@users.noreply.github.com>
2022-12-23 15:07:13 +0100

    upload theme_lenp

---
+++ theme_lenp/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Clean LenP',
+    'description': 'LenP Theme',
+    'category': 'Theme/Services',
+    'summary': 'Legal, Corporate, Business, Tech, Services',
+    'sequence': 120,
+    'version': '2.1.0',
+    'author': 'GripICT',
+    'depends': ['theme_common'],
+    'data': [
+        'data/ir_asset.xml',
+        
+    ],
+
+    'license': 'LGPL-3',
+    'assets': {
+        
+    }
+}
